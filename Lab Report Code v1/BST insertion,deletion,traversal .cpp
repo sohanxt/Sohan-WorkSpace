f5492ac2d80de967c2341eb183918e98315cf301
ABU SAHID SOHAN <154542799+sohanxt@users.noreply.github.com>
2024-11-06 01:16:58 +0600

    Add files via upload

---
+++ Lab Report Code v1/BST insertion,deletion,traversal .cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+Node* insert(Node* root, int data) {
+    if (root == nullptr) {
+        return new Node(data);
+    }
+    if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else if (data > root->data) {
+        root->right = insert(root->right, data);
+    }
+    return root;
+}
+
+void inorder(Node* root) {
+    if (root != nullptr) {
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+}
+
+Node* findMin(Node* root) {
+    while (root->left != nullptr) {
+        root = root->left;
+    }
+    return root;
+}
+
+Node* deleteNode(Node* root, int data) {
+    if (root == nullptr) {
+        return root;
+    }
+    if (data < root->data) {
+        root->left = deleteNode(root->left, data);
+    } else if (data > root->data) {
+        root->right = deleteNode(root->right, data);
+    } else {
+        if (root->left == nullptr) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        } else if (root->right == nullptr) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        Node* temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+int main() {
+    Node* root = nullptr;
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 70);
+    root = insert(root, 60);
+    root = insert(root, 80);
+
+    cout << "Inorder traversal of the BST: ";
+    inorder(root);
+    cout << endl;
+
+    root = deleteNode(root, 20);
+    cout << "Inorder traversal after deleting 20: ";
+    inorder(root);
+    cout << endl;
+
+    root = deleteNode(root, 30);
+    cout << "Inorder traversal after deleting 30: ";
+    inorder(root);
+    cout << endl;
+
+    root = deleteNode(root, 50);
+    cout << "Inorder traversal after deleting 50: ";
+    inorder(root);
+    cout << endl;
+
+    return 0;
+}
